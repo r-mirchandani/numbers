de7c7fa60113a3bd418bddf04488e4805474ede8
Rohan Mirchandani <rohanmirchandani@Rohans-MacBook-Pro.local>
2016-11-20 17:43:09 +1000

    Optimisation and HTML serving

--- src/main/java/com/num2string/ConversionController.java
+++ src/main/java/com/num2string/ConversionController.java
@@ -4,26 +4,34 @@ package com.num2string;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.stereotype.Controller;
 
 @CrossOrigin
-@RestController
+@Controller
 public class ConversionController { // Object model controller to route requests
 
     // Initialize resources to be used by the server
     NumMap numMap = new NumMap();
     Converter conversion = new Converter(numMap);
 
+    // GET request handler to serve index.html
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(){
+        return "index.html";
+    }
+
     // GET request to extract output string
-    @RequestMapping(value = "/pull", method =  RequestMethod.GET)
-    public String index() {
+    @RequestMapping(value = "/pull", method = RequestMethod.GET)
+    public @ResponseBody String Pull(){
         return conversion.getOutput();
     }
 
     // POST request to send input string to server
     @RequestMapping(value = "/push", method = RequestMethod.POST)
-    public void Convert(@RequestParam String input){
+    public @ResponseBody void Convert(@RequestParam String input){
         conversion.setInput(input);
         conversion.Convert();
     }
